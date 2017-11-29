60da32ccd1ec73097b9f689a19fede7836ea043a
TevinStuurland <tevin94@hotmail.com>
2017-11-29 10:59:19 +0100

    Initial generated site

---
+++ wp-login.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="nl">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="nl">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Inloggen &lsaquo; trackshipping &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='https://troop-design.github.io/topshipping/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-nl-nl">
+		<div id="login">
+		<h1><a href="https://nl.wordpress.org/" title="Mogelijk gemaakt door WordPress" tabindex="-1">Mogelijk gemaakt door WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="https://troop-design.github.io/topshipping/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Gebruikersnaam of e-mailadres<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Wachtwoord<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Onthoud mij</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Inloggen" />
+		<input type="hidden" name="redirect_to" value="https://troop-design.github.io/topshipping/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="https://troop-design.github.io/topshipping/wp-login.php?action=lostpassword">Wachtwoord vergeten?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="https://troop-design.github.io/topshipping/">&larr; Terug naar trackshipping</a></p>
+	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
